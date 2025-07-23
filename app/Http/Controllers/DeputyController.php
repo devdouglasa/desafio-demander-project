36c32b34019ed8c9521f23c9a676bf1ed388274f
devdouglasa <douglasalisson27@gmail.com>
2025-07-23 19:39:45 -0300

    feat: created deputy controller

---
+++ app/Http/Controllers/DeputyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Expense;
+use Illuminate\Http\Request;
+use App\Models\Deputie;
+
+class DeputyController extends Controller
+{
+    function index(Request $request)
+    {
+        $uf = $request['uf'];
+        $party = $request['party'];
+
+
+        $query = Deputie::query();
+
+        if ($uf)
+        {
+            $query->where('sigla_uf', $uf);
+        }
+
+        if ($party)
+        {
+            $query->where('sigla_partido', $party);
+        }
+
+        $deputies = $query->get();
+
+        $ufs = Deputie::select('sigla_uf')->distinct()->orderBy('sigla_uf')->pluck('sigla_uf');
+
+        $parties = Deputie::select('sigla_partido')->distinct()->orderBy('sigla_partido')->pluck('sigla_partido');
+
+        return view('deputies.deputies', compact('deputies', 'uf', 'party', 'ufs', 'parties'));
+    }
+
+    function show(int $id)
+    {
+        $year = request('year') ?? date('Y');
+        $month = request('month');
+
+        $deputy = Deputie::findOrFail($id);
+
+        $query = Expense::where('deputado_id', $id)->where('ano', $year);
+
+        if ($month) {
+            $query->where('mes', $month);
+        }
+
+        $expenses = $query->get();
+
+        $teste = Expense::get()->where('deputado_id', 204560);
+        
+        dd($teste->toArray());
+
+        $totalExpenses = $expenses->sum('valor_documento');
+
+
+        $expensesForType = $expenses->groupBy('tipo_despesa')->map(function ($items) {
+            return $items->sum('valor_documento');
+        });
+
+        return view('deputies.deputy', compact('deputy', 'expenses', 'totalExpenses', 'expensesForType'));
+    }
+}
